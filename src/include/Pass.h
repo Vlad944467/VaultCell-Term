0ac7a2a403e98c588546fee799e0dbc316b0dd18
Vlad944467 <vladislav58387v@gmail.com>
2026-08-28 23:34:54 +0500

    Create Pass.h

---
+++ src/include/Pass.h
@@ -0,0 +1,13 @@
+#ifndef PASS_H
+#define PASS_H
+
+extern char site1[100];
+extern char login1[60];
+extern char password1[60];
+extern char site2[100];
+extern char login2[60];
+extern char password2[60];
+
+void pass();
+
+#endif
