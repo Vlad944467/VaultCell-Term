cd16bd06f24014e2b462d003e3d112358e279dbd
Vlad944467 <vladislav58387v@gmail.com>
2026-08-28 23:33:18 +0500

    Create Bcell.h

---
+++ src/Bcell.h
@@ -0,0 +1,15 @@
+#ifndef BCELL_H
+#define BCELL_H
+
+extern char book1[100];
+extern char author1[60];
+extern char year1[30];
+extern char book2[100];
+extern char author2[60];
+extern char year2[30];
+extern char Binp1[10000000];
+extern char Binp2[10000000];
+
+void Bcell_();
+
+#endif
