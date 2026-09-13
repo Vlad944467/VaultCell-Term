b7604a0a54384e14b253fdfcef616ef42717ad00
Vlad944467 <vladislav58387v@gmail.com>
2026-09-13 13:19:31 +0500

    Update in.h

--- src/include/in.h
+++ src/include/in.h
@@ -1,5 +1,14 @@
 #ifndef IN_H
 #define IN_H
+void mov();
+void add();
+void INT_print();
+void sub();
+void add_num();
+void subt_num();
+void clear();
+void mul_num();
+void div_num();
 
 void raz();
 void inter();
