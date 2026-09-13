9ba6ec6a1dedbf3bb6d0ac04009fe5b9a98cfd79
Vlad944467 <vladislav58387v@gmail.com>
2026-09-13 15:18:50 +0500

    Update build_run.sh

--- build_run.sh
+++ build_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -o src/VaultCell src/CELL.c src/Pass.c src/Bcell.c src/SIL.c -Isrc/include
-./src/VaultCell
+gcc -o src/VaultCell src/CELL.c src/Pass.c src/Bcell.c src/SIL/SIL.c -Isrc/include
+./src/VaultCell-Term
